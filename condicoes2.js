5004b33609b000f332b1e222947fc4e079116d23
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-02-25 12:11:46 -0300

    Add files via upload

---
+++ condicoes2.js
@@ -0,0 +1,16 @@
+// exercício 1:
+let valor1 = 50 
+let valor2 = 35
+let Mult = valor1 * valor2
+console.log("Soma = " + soma)
+
+
+
+// exercício 2:
+let av1 = 10.0
+let av2 = 9.0
+let av3 = 6.0
+
+let div = av1 / av2 / av3
+
+console.log("Multiplicação = " + Mult)
