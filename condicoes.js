93fe3fe2fd417e7f63abf8f6ba797e0dfd9160c2
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-02-25 11:50:50 -0300

    Add files via upload

---
+++ condicoes.js
@@ -0,0 +1,15 @@
+//CONDIÇÕES
+
+let idade = 27
+let possuiCNH = true // false
+if (idade >= 18){
+    if (possuiCNH == true) {
+        console.log("Você pode dirigir!")
+    }
+        console.log("Você pode tirar CNH")
+} else {
+    console.log("Não pode!")
+}
+    
+ 
+ 
