302b1e8ac00a2fde08301c34331c31494e593fa6
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-03-10 08:16:43 -0300

    Update condicoes2.js

--- condicoes2.js
+++ condicoes2.js
@@ -1,16 +1,17 @@
 // exercício 1:
-let valor1 = 50 
-let valor2 = 35
-let Mult = valor1 * valor2
-console.log("Soma = " + soma)
-
+const peso = 53;
+const agua_ml = peso * 35;
+const agua_l = agua_ml/1000;
+console.log("a quantidade ideal de agua é" + agua_l + "litros")
 
 
 // exercício 2:
 let av1 = 10.0
-let av2 = 9.0
-let av3 = 6.0
-
-let div = av1 / av2 / av3
-
-console.log("Multiplicação = " + Mult)
+let av2 = 70.0
+let av3 = 40.0
+const media = ( av1 + av2 + av3 )/3
+if(media>=6.0){
+    console.log("aprovado");
+}else{
+    console.log("reprovado");
+}
