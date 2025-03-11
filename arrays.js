961beaa2ecde4e75b655d06d2360b6893a1f6343
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-03-11 11:31:55 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,15 @@
+// LISTAS
+//Lista de Strings:
+const lanches = ["x-egg", "x-burguer", "x-bancon", "x-tudo", "x-frango"];
+// Lista de Number:
+const precos = [12, 15, 15, 20, 12];
+console.log(lanches);
+console.log(precos);
+console.log(lanches[0] + " - R$ " + precos[0])
+console.log(lanches[1] + " - R$ " + precos[1])
+console.log(lanches[2] + " - R$ " + precos[2])
+console.log(lanches[3] + " - R$ " + precos[3])
+console.log(lanches[4] + " - R$ " + precos[4])
+
+const tamanho = lanches.length;
+console.log(tamanho);                                                    
