b41671bd799cbb5c4a243fa6ee919b98dac7a476
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-03-17 08:45:14 -0300

    Add files via upload

---
+++ lacos.js
@@ -0,0 +1,20 @@
+let custos = [800,1200,800,500];
+let contador = 0
+let somatorio = 0
+while (contador < custos.length){
+    somatorio = somatorio + custos[contador];
+    contador = contador + 1
+}
+console.log("o seu custo mensal é" + somatorio);
+
+
+
+let ganhos = [3000,100];
+let contadorsalario = 0
+let somatoriosalario = 0
+while (contadorsalario < custos.length){
+    somatoriosalario = somatoriosalario + custos[contadorsalario];
+    contadorsalario = contadorsalario + 1
+}
+console.log("o seu custo mensal é" + somatoriosalario);
+
