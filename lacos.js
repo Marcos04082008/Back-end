2d4fd9fd1af0645a848993befa0fda3b42381ecb
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-03-18 11:14:50 -0300

    Update lacos.js

--- lacos.js
+++ lacos.js
@@ -1,20 +1,21 @@
 let custos = [800,1200,800,500];
-let contador = 0
-let somatorio = 0
-while (contador < custos.length){
-    somatorio = somatorio + custos[contador];
-    contador = contador + 1
+let contador = 0 ;
+let somatorio = 0 ;
+while (contador<custos.length){
+       somatorio = somatorio + custos[contador]
+       contador = contador + 1
 }
-console.log("o seu custo mensal é" + somatorio);
+console.log("O seu custo mensal é " + somatorio);
 
-
-
-let ganhos = [3000,100];
-let contadorsalario = 0
-let somatoriosalario = 0
-while (contadorsalario < custos.length){
-    somatoriosalario = somatoriosalario + custos[contadorsalario];
-    contadorsalario = contadorsalario + 1
+let salario = [3000,100];
+contador = 0 ;
+let somatoriosalario = 0 ;
+while (contador<salario.length){
+       somatoriosalario = somatoriosalario + salario[contador]
+       contador = contador + 1
 }
-console.log("o seu custo mensal é" + somatoriosalario);
+console.log("O seu salario mensal é " + somatoriosalario);
+
+let saldo = somatoriosalario-somatorio
+console.log ("Seu saldo mensal é " + saldo)
 
