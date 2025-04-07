f34d8fe37e43398910e015e818dad4c6478ae5c2
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-04-07 08:31:42 -0300

    Create jogadores.js

---
+++ jogadores.js
@@ -0,0 +1,24 @@
+let times = [
+    {
+        nome: "Flamengo",
+        goleiro: "Mtheus Cunha Queiroz",
+        alaDireita: "Otavio oliveira dos Santos",
+        alaEsquerda: "Alex Sandro",
+        pivo: "Taya Gallizzi",
+        fixo: "Gabigol",
+        tecnico: "Filipe Luis",
+        vitorias: "53",
+        derrotas: "26",
+    },
+    {
+        nome: "Flamengo",
+        goleiro: "Mtheus Cunha Queiroz",
+        alaDireita: "Otavio oliveira dos Santos",
+        alaEsquerda: "Alex Sandro",
+        pivo: "Taya Gallizzi",
+        fixo: "Gabigol",
+        tecnico: "Filipe Luis",
+        vitorias: "53",
+        derrotas: "26",
+    },
+]
