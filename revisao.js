79b20f6b294a57a38354fbc1d04eca2de1690f09
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-05-13 11:59:32 -0300

    Add files via upload

---
+++ revisao.js
@@ -0,0 +1,26 @@
+const meuNome = "Marcos Vinicius";
+let numerochamada = 18;
+
+console.log ("Meu nome é:", meuNome);
+console.log ("meu numero da chamada é:", numerochamada);
+
+const Jogos = [
+"amarelinha",
+"xadrex",
+"volei",
+];
+
+let jogos = ['amarelinha' , 'xadrez' , 'volei' ];
+let contador = 0;
+let somatorio = 0;
+console.log ( ""+ jogos [contador]);
+while(contador < 3){
+    console.log ("" + jogos [contador]);
+    contador++;
+};
+
+    const meunome = "Marcos Vinicius";
+    const filme = "percy Jackson";
+    const ano = "2005";
+
+
