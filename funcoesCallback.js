7cf69bb4884077a95cdceacfee1c0d351811cce8
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-05-19 08:37:43 -0300

    Update funcoesCallback.js

--- funcoesCallback.js
+++ funcoesCallback.js
@@ -2,3 +2,8 @@ function media(nota1, nota2){
     return(nota1+nota2)/2
 }
 console.log(media(5.2, 0))
+
+mediaArrow = (nota1, nota2) => {
+    return (nota1+nota2)/2
+}
+console.log(mediaArrow(10, 9))
