db93b25ba1572e07b7739c31f2b6f0e3c81facb7
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-08-05 12:05:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const port = 3000
+app.use(express.json())
+
+app.get("/bemvindo", (req, res)=>{
+    res.send("Olá, seja bem vindo a API")
+})
+
+app.post("/bemvindo", (req, res)=>{
+    const dados = req.body
+    res.send("Olá, seja bem vindo " + dados.nome)
+})
+app.listen(port, ()=>{
+    console.log("API RODANDO NA PORTA" + port)
+})
+ 
