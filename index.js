6619f7696d2bb9b1e29483cfefd334bf61fbad34
Marcos04082008 <162449987+Marcos04082008@users.noreply.github.com>
2025-08-12 11:35:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,20 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
-app.get("/bemvindo", (req, res)=>{
-    res.send("Olá, seja bem vindo a API")
+
+const usuarios = []
+app.post("/usuarios", (req, res)=>{
+    const usuario = req.body
+    usuarios.push(usuario)
+    res.send("Usuário cadastrado com sucesso!")
 })
 
-app.post("/bemvindo", (req, res)=>{
-    const dados = req.body
-    res.send("Olá, seja bem vindo " + dados.nome)
+app.get("/usuarios", (req, res)=> {
+    res.send (usuarios)
 })
+
 app.listen(port, ()=>{
-    console.log("API RODANDO NA PORTA" + port)
+    console.log("API RODANDO NA PORTA"+ port)
 })
 
 
